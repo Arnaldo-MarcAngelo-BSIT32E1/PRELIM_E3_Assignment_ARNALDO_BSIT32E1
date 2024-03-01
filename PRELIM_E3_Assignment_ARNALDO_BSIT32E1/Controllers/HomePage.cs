7b350777e385ebed2083a8d30dd53f68c28d7c41
Gelo <Gelo@LAPTOP-Q1ET7JAV>
2024-03-01 19:45:35 +0700

    Add project files.

---
+++ PRELIM_E3_Assignment_ARNALDO_BSIT32E1/Controllers/HomePage.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PRELIM_E3_Assignment_ARNALDO_BSIT32E1.Controllers
+{
+    public class HomePage : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
